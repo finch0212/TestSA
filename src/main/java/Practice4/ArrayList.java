ad72b4da024a5d9400300c3f64442afde8826b41
finch0212 <sal0212@mail.ru>
2018-07-11 23:02:18 +0300

    arrayList is done

--- src/main/java/Practice4/ArrayList.java
+++ src/main/java/Practice4/ArrayList.java
@@ -5,73 +5,120 @@ public class ArrayList implements List {
     private int capacity;
     private Object[] mass;
 
-    ArrayList(){
+    ArrayList() {
         size = 0;
         capacity = 10;
         mass = new Object[capacity];
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public boolean isEmpty(){
-        return size==0;
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-    public boolean contains(Object item){
-        for (Object o:mass) {
-            if(o==item)return true;
+    public boolean contains(Object item) {
+        for (Object o : mass) {
+            if (o == item) return true;
         }
         return false;
     }
 
-    public boolean add(Object item){
-        if(size<capacity){
+    public boolean add(Object item) {
+        if (size < capacity) {
             mass[size++] = item;
-        }
-        else{
-            capacity *= 1.5;
-            Object[] tmpmass = new Object[capacity];
-            for(int i = 0; i < size; i++){
-                tmpmass[i] = mass[i];
-            }
-            tmpmass[size++]=item;
-            mass = tmpmass;
+        } else {
+            capacityIncrease();
+            mass[size++] = item;
         }
         return true;
     }
-    public boolean remove(Object item){
-        return  true;
-    }
-    public void clear(){
 
+    public boolean remove(Object item) {
+        for (int i = 0; i < size; i++) {
+            if (mass[i] == item) {
+                for (int j = i; j < size - 1; j++) {
+                    mass[j] = mass[j + 1];
+                }
+                size--;
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void add(int index, Object item){
-
+    public void clear() {
+        size = 0;
+        capacity = 10;
+        mass = new Object[capacity];
     }
-    public void set(int index, Object item){
 
+    public void add(int index, Object item) {
+        if (index == size) {
+            add(item);
+            return;
+        } else if (index < size) {
+            if (size == capacity) capacityIncrease();
+            for (int i = size; i > index; i--) {
+                mass[i] = mass[i - 1];
+            }
+            mass[index] = item;
+            size++;
+        } else {
+            //если нет индекса?////////////////////////////////////////////////////////////////////////////
+        }
     }
-    public Object get(int index){
+
+
+    public void set(int index, Object item) {
+        mass[index] = item;
+        //если нет индекса?////////////////////////////////////////////////////////////////////////////
+    }
+
+    public Object get(int index) {
         return mass[index];
     }
-    public int indexOf(Object item){
-        return  0;
+
+    public int indexOf(Object item) {
+        for (int i = 0; i < size; i++) {
+            if (mass[i] == item) return i;
+        }
+        return -1;/////////////////////////////////////////////////////////////////////////////////////
     }
-    public int lastIndexOf(Object item){
-        return  0;
-    }
-    public void remove(int index){
+
+    public int lastIndexOf(Object item) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (mass[i] == item) return i;
+        }
+        return -1;/////////////////////////////////////////////////////////////////////////////////////
 
     }
-    public List subList(int from, int to){
-        return  null;
+
+    public void remove(int index) {
+        for (int i = index; i < size-1; i++) {
+            mass[i] = mass[i + 1];
+        }
+        size--;
+
+    }
+
+    public List subList(int from, int to) {
+        return null;
     }
 
     public int getCapacity() {
         return capacity;
+    }
+
+    private void capacityIncrease() {
+        capacity *= 1.5;
+        Object[] tmpmass = new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            tmpmass[i] = mass[i];
+        }
+        mass = tmpmass;
     }
 }
 
--- src/main/java/Practice4/CheckArrList.java
+++ src/main/java/Practice4/CheckArrList.java
@@ -5,12 +5,22 @@ public class CheckArrList {
     public static void main(String[] args){
         ArrayList mass = new ArrayList();
         printArr(mass);
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 15; i++){
             mass.add(i);
             printArr(mass);
             System.out.println();
         }
-
+        mass.add(0,100);
+        mass.add(16,160);
+        printArr(mass);
+        mass.remove(0);
+        printArr(mass);
+        mass.remove((Integer)1);
+        printArr(mass);
+        mass.set(12,5);
+        printArr(mass);
+        System.out.println(mass.indexOf(5));
+        System.out.println(mass.lastIndexOf(5));
     }
 
     public static void printArr(ArrayList mass){
@@ -18,7 +28,7 @@ public class CheckArrList {
         System.out.println("Cap = "+mass.getCapacity());
         System.out.print("Mass: ");
         for(int i = 0; i < mass.size; i++){
-            System.out.print(mass.get(i));
+            System.out.print(mass.get(i) + " ");
         }
         System.out.println();
     }
